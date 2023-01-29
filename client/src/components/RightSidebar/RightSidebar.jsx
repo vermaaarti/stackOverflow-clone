0d8c1e5d738a0038f666b7751389e0e8a900dc32
vermaaarti <av3001560@gmail.com>
2023-01-29 22:41:55 +0530

    added the right side bar

--- client/src/components/RightSidebar/RightSidebar.jsx
+++ client/src/components/RightSidebar/RightSidebar.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import './RightSidebar.css'
+import Widget from './Widget'
+import WidgetTags from './WidgetTags'
 
 const RightSidebar = () => {
   return (
-    <div>RightSidebar</div>
+    <aside className='right-sidebar'>
+    <Widget />
+    <WidgetTags/>
+    </aside>
   )
+
 }
 
 export default RightSidebar
