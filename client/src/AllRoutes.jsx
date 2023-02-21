68dbb8b9615a9081c07fa34b7dcc4145f6e45297
vermaaarti <av3001560@gmail.com>
2023-02-22 01:14:12 +0530

    main-bar part-1 [18:52]

--- client/src/AllRoutes.jsx
+++ client/src/AllRoutes.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import {Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Auth from './pages/Auth/Auth'
+import Questions from './Pages/Questions/Questions'
 
 const AllRoutes = () => {
   return (
     <Routes>
          <Route exact path='/' component={<Home />} /> 
         <Route path='/Auth' element={<Auth />} />
+        <Route path='/Questions' element={<Questions />} />
     </Routes>
   )
 }
