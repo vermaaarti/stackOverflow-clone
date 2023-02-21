4fc0146b00cd486311a5a549f4529fcc5eb75f12
vermaaarti <av3001560@gmail.com>
2023-02-21 14:54:40 +0530

    added home, left and right side bar and at [46:13]

---
+++ client/src/components/RightSidebar/Widget.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './RightSidebar.css'
+import comment from '../../assets/comment.svg'
+import pen from '../../assets/pen.svg'
+import blacklogo from '../../assets/blacklogo.svg'
+
+const Widget = () => {
+  return (
+    <div>Widget</div>
+  )
+}
+
+export default Widget
